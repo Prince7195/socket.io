616b333868314bfa4057ecb4cbaf17c5573f1f66
Vijayasekhar Deepak <vijayasekhar.deepak@walmartlabs.com>
2020-12-26 12:21:11 +0530

    SOCKET201

--- socketio201/chatOld.js
+++ socketio201/chatOld.js
@@ -15,11 +15,21 @@ app.get('/', function (req, res) {
 io.on("connection", socket => {
     socket.emit("messageFromServer", { data: "Welcome to the socketio server" });
     socket.on("messageToServer", msg => {
-        console.log(msg);
     });
+    socket.on("newMessageToServer", msg => {
+        // [socket.emit("messageToClients", { text: msg.text });
+        // socket.broadcast.emit('messageToClients', { text: msg.text });]
+        // or
+        //io.emit('messageToClients', { text: msg.text });
+        // or
+        io.of("/").emit('messageToClients', { text: msg.text });
+
+        setTimeout(() => {
+            io.of("/admin").emit('welcome', "Welcome to Admin namespace(workspace)! from main namespace.");
+        }, 5000);
+    })
 });
 
 io.of("/admin").on("connection", socket => {
-    console.log("Someone connected to the admin namespace");
     io.of("/admin").emit("welcome", "Welcome to Admin namespace(workspace)!");
 })
